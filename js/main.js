89ae8c69effa34bdae665ae7c75a3f06346faff9
cameronclass <kamrankutumov@gmail.com>
2022-05-19 14:43:08 +0300

    initial commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
-// Preloader
-/* $( function() {
-    var $preloader = $('#page-preloader'),
-        $spinner   = $preloader.find('.spinner');
-    $spinner.fadeOut();
-    $preloader.delay(350).fadeOut('slow');
-}); */
+const hamburger = document.querySelectorAll(".hamburger");
+const headerMenu = document.querySelector(".header__menu");
+hamburger.onclick = function () {
+  hamburger.classList.toggle("is-active");
+};
+
+hamburger.forEach((item) => {
+  item.onclick = function () {
+    headerMenu.classList.toggle("header__menu_active");
+  };
+});
